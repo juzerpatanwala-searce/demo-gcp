52ab410a48e1395edd7845d21457dc9f41383de5
juzer_patanwala_searce_com <juzer.patanwala@searce.com>
2024-08-02 10:19:19 +0000

    made changes

--- app2/app.py
+++ app2/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'App deployed on Cloud Run!!!'
+    return 'App deployed on Cloud Run!'
 
 if __name__ == '__main__':
     # Listen on all interfaces and port 80
